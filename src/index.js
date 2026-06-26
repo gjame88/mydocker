d9d2a16029654a0540c3a4019bfdf68974ab0345
james <kangjaeku.g@gmail.com>
2026-06-26 16:12:28 +0900

    my1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const message = "Hello, Nginx Deployment!";
+const message = "Hello2, Nginx Deployment!";
 console.log(message);
 document.addEventListener("DOMContentLoaded", () => {
   document.body.innerHTML = `<h1>${message}</h1>`;
